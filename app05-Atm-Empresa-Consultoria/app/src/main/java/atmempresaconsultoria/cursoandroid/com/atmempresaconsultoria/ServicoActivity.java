b160f263025c81222d417c92d10635cb86b379b1
raphaelratuczne <raphaelratuczne@gmail.com>
2017-06-03 23:41:16 -0300

    add app05

---
+++ app05-Atm-Empresa-Consultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/ServicoActivity.java
@@ -0,0 +1,16 @@
+package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServicoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servico);
+
+
+    }
+}
