d6de3665a50f1671c616221520625e95e8b675aa
raphaelratuczne <raphaelratuczne@gmail.com>
2017-06-04 21:19:05 -0300

    add app06

---
+++ app06-Cara-ou-Coroa/app/src/main/java/caraoucoroa/cursoandroid/com/caraoucoroa/MainActivity.java
@@ -0,0 +1,40 @@
+package caraoucoroa.cursoandroid.com.caraoucoroa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView botaoJogar;
+    private String[] opcoes = {"cara", "coroa"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (ImageView) findViewById(R.id.botaoJogarId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // gerar numero aleatorio
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(2);
+
+                // 0 - cara, 1 - coroa
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                intent.putExtra("opcao", opcoes[numeroAleatorio]);
+
+                startActivity( intent );
+            }
+        });
+    }
+}
